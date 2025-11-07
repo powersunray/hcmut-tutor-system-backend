536517c3d09bd65c361e1aa88de3e2e6fef9ac96
powersunray <iamvietnam@gmail.com>
2025-11-07 23:06:06 +0700

    Add .exception package with some simple exceptions

---
+++ src/main/java/hcmut/edu/vn/tutor_support_system/exception/SlotUnavailableException.java
@@ -0,0 +1,7 @@
+package hcmut.edu.vn.tutor_support_system.exception;
+
+public class SlotUnavailableException extends RuntimeException {
+    public SlotUnavailableException(String message) {
+        super(message);
+    }
+}
