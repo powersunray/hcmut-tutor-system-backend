bf29efe2737fc1a92201c5240ae5fa018ebb154c
Tyler <atunguye25@gmail.com>
2025-11-26 00:36:40 +0700

    chore: remove test formatting file

--- src/main/java/hcmut/edu/vn/tutor_support_system/TestFormatting.java
+++
@@ -1,8 +0,0 @@
-package hcmut.edu.vn.tutor_support_system;
-
-public class TestFormatting {
-  public void badlyFormattedMethod() {
-    String test = "hello world";
-    System.out.println(test);
-  }
-}
