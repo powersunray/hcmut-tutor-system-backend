587b200aa324807541fc9c4f1660b0a0a40438a5
Tyler <atunguye25@gmail.com>
2025-11-26 00:35:35 +0700

    test: verify pre-commit hook formatting with Spotless

---
+++ src/main/java/hcmut/edu/vn/tutor_support_system/TestFormatting.java
@@ -0,0 +1,8 @@
+package hcmut.edu.vn.tutor_support_system;
+
+public class TestFormatting {
+  public void badlyFormattedMethod() {
+    String test = "hello world";
+    System.out.println(test);
+  }
+}
